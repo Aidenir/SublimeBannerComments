d209a622684336f5617bb25a97a3cc47bc7498b7
Viktor Hansson <viktor.hansson@me.com>
2013-12-19 10:20:41 +0100

    Now takes the selected text and replaces it with the comment

--- BannerComments.py
+++ BannerComments.py
@@ -2,13 +2,16 @@ import sublime, sublime_plugin
 import urllib.request
 from html.parser import HTMLParser
 
+comment = ""
 
 class Makebanner(sublime_plugin.TextCommand):
 	def run(self,edit):
+		global comment
+		comment = ""
 		view = self.view
 		sel = view.sel()
 		selection = view.substr(sel[0])
-		
+
 		url = 'http://www.network-science.de/ascii/ascii.php?TEXT=' + selection + '&x=24&y=9&FONT=banner3&RICH=no&FORM=left&STRE=no&WIDT=80'
 		response = urllib.request.urlopen(url)
 		data = response.read()      # a `bytes` object
@@ -16,9 +19,12 @@ class Makebanner(sublime_plugin.TextCommand):
 		parser = MyHTMLParser(strict=False)
 		parser.feed(text)
 
+		view.replace(edit,sel[0],comment)
 
 
 class MyHTMLParser(HTMLParser): 
 	def handle_data(self, data):
 		if data[0] == '#':
+			global comment
+			comment = data
 			print(data)
